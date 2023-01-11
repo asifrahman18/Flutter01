a73ec6b01d0bcb36b3d939e381c9afa3e4f813d5
Asif Rahman <57532856+asifrahman18@users.noreply.github.com>
2023-01-11 19:16:31 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+//importing 'flutter material' package
 import 'package:flutter/material.dart';
 
-void main() {}
+// Main function
+void main() {
+  //runApp function obtained from material package runs the class0
+  runApp(MyApp());
+}
 
+// StatelessWidget class obtained from material package
 class MyApp extends StatelessWidget {
+  //Widget return type, build function from material package
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Text('Hello!'),
