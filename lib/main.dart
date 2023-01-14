0fa13bf47ab34ba173eb63194e7dc42fdba739d1
Asif Rahman <57532856+asifrahman18@users.noreply.github.com>
2023-01-14 15:12:11 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,24 @@ void main() {
   runApp(MyApp());
 }
 
+//Alternate way of calling main function
+
+// void main() => runApp(MyApp());
+
 // StatelessWidget class obtained from material package
 class MyApp extends StatelessWidget {
   //Widget return type, build function from material package
+  @override
+
+  // ctrl + Space brings the list of possible arguments in any function
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Lit App'),
+        ),
+        body: Text('Default Text'),
+      ),
     );
   }
 }
